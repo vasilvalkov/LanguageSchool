ee0a43fb4d5417c2b2fd673e9299cb1dfcf25e2f
vasilvalkov <vasil.valkov@gmail.com>
2017-09-25 22:07:12 +0300

    Changed MS test to NUnit

--- LanguageSchoolApp/LanguageSchoolApp.UnitTests/Controllers/HomeControllerTest.cs
+++ LanguageSchoolApp/LanguageSchoolApp.UnitTests/Controllers/HomeControllerTest.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LanguageSchoolApp;
 using LanguageSchoolApp.Controllers;
+using NUnit.Framework;
 
 namespace LanguageSchoolApp.UnitTests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class HomeControllerTest
     {
-        [TestMethod]
+        [Test]
         public void Index()
         {
             // Arrange
@@ -25,7 +25,7 @@ namespace LanguageSchoolApp.UnitTests.Controllers
             Assert.IsNotNull(result);
         }
 
-        [TestMethod]
+        [Test]
         public void About()
         {
             // Arrange
@@ -38,7 +38,7 @@ namespace LanguageSchoolApp.UnitTests.Controllers
             Assert.AreEqual("Your application description page.", result.ViewBag.Message);
         }
 
-        [TestMethod]
+        [Test]
         public void Contact()
         {
             // Arrange
