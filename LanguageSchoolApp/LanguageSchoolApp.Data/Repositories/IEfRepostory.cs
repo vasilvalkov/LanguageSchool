25428c4f90bb9b40c4212c80048e5e88fd0bb3a2
vasilvalkov <vasil.valkov@gmail.com>
2017-09-28 17:12:26 +0300

    Added EfRepository to abstract data access

---
+++ LanguageSchoolApp/LanguageSchoolApp.Data/Repositories/IEfRepostory.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using LanguageSchoolApp.Data.Model.Contracts;
+
+namespace LanguageSchoolApp.Data.Repositories
+{
+    public interface IEfRepostory<T>
+        where T : class, IDeletable
+    {
+        IQueryable<T> All { get; }
+
+        IQueryable<T> AllAndDeleted { get; }
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+    }
+}
