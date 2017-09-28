ffbc2dd71d07854081cbf6f48b72aaa02a555c11
vasilvalkov <vasil.valkov@gmail.com>
2017-09-28 23:50:40 +0300

    Seed some sample data in db

--- LanguageSchoolApp/LanguageSchoolApp.Data/Migrations/Configuration.cs
+++ LanguageSchoolApp/LanguageSchoolApp.Data/Migrations/Configuration.cs
@@ -3,12 +3,16 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using LanguageSchoolApp.Data.Model;
 using System.Data.Entity.Migrations;
 using System.Linq;
+using System;
 
 namespace LanguageSchoolApp.Data.Migrations
 {
 
     public sealed class Configuration : DbMigrationsConfiguration<MsSqlDbContext>
     {
+        const string AdministratorUserName = "admin@linguana.com";
+        const string AdministratorPassword = "aaSS11!!";
+
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = false;
@@ -17,9 +21,51 @@ namespace LanguageSchoolApp.Data.Migrations
 
         protected override void Seed(MsSqlDbContext context)
         {
-            const string AdministratorUserName = "admin@linguana.com";
-            const string AdministratorPassword = "aaSS11!!";
+            this.SeedUsers(context);
+            this.SeedSampleData(context);
+            base.Seed(context);
+        }
 
+        private void SeedSampleData(MsSqlDbContext context)
+        {
+            if (!context.Courses.Any())
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var date = new DateTime(2017, 7 + i, 10 + i * 2);
+                    var course = new Course()
+                    {
+                        Title = " Sample Course " + i,
+                        Description = "Lorem ipsum dolor sit amet, ei explicari voluptaria cum, vel epicurei theophrastus in. Mel an elit eleifend iracundia, id erat antiopam inimicus pri. Partiendo erroribus ad sed, dolor dictas vocent ne usu, per eu facer errem. Cum nostrum perfecto cu, eos ei essent feugiat eleifend. Vis et nostrum percipit.",
+                        CreatedOn = DateTime.Now,
+                        StartsOn = date,
+                        EndsOn = date.AddDays(30)
+                    };
+
+                    course.Students.Add(context.Users.First(u => u.Email == AdministratorUserName));
+
+                    context.Courses.Add(course);
+                }
+
+                if (!context.CourseResults.Any())
+                {
+                    var courseResult = new CourseResult()
+                    {
+                        Course = context.Courses.First(c => c.Title == "Sample Course 2"),
+                        CoursePoints = 75
+                    };
+
+                    context
+                        .Users
+                        .First(u => u.Email == AdministratorUserName)
+                        .CourseResults
+                        .Add(courseResult);
+                }
+            }
+        }
+
+        private void SeedUsers(MsSqlDbContext context)
+        {
             if (!context.Roles.Any())
             {
                 var roleStore = new RoleStore<IdentityRole>(context);
@@ -29,9 +75,15 @@ namespace LanguageSchoolApp.Data.Migrations
 
                 var userStore = new UserStore<User>(context);
                 var userManager = new UserManager<User>(userStore);
-                var user = new User { UserName = AdministratorUserName, Email = AdministratorUserName, EmailConfirmed = true };
-                userManager.Create(user, AdministratorPassword);
+                var user = new User
+                {
+                    UserName = AdministratorUserName,
+                    Email = AdministratorUserName,
+                    EmailConfirmed = true,
+                    CreatedOn = DateTime.Now
+                };
 
+                userManager.Create(user, AdministratorPassword);
                 userManager.AddToRole(user.Id, "Admin");
             }
         }
