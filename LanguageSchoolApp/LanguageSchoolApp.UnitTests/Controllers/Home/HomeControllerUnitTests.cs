8d20a1ef0e6f0630361147748f9cb123ca59c956
vasilvalkov <vasil.valkov@gmail.com>
2017-10-02 23:48:30 +0300

    Add tests for HomeController Index action

---
+++ LanguageSchoolApp/LanguageSchoolApp.UnitTests/Controllers/Home/HomeControllerUnitTests.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using LanguageSchoolApp.Controllers;
+using LanguageSchoolApp.Data.Model;
+using LanguageSchoolApp.Models.Home;
+using LanguageSchoolApp.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LanguageSchoolApp.UnitTests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index_ShouldReturnNonNullResult()
+        {
+            // Arrange
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Course, CourseTileViewModel>();
+                cfg.CreateMap<CourseTileViewModel, Course>();
+            });
+            var courseServiceStub = new Mock<ICourseService>();
+            var mapperStub = new Mock<IMapper>();
+
+            HomeController controller = new HomeController(courseServiceStub.Object, mapperStub.Object);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Index_ViewModel_ShouldHaveExactlyThreeUpcomingCourses()
+        {
+            // Arrange
+            var listOfCourses = new List<Course>()
+            {
+                new Course() { StartsOn = new DateTime(5540,2,2)},
+                new Course() { StartsOn = new DateTime(5540,2,2)},
+                new Course() { StartsOn = new DateTime(5540,2,2)},
+                new Course() { StartsOn = new DateTime(5540,2,2)}
+            };
+
+            var courseServiceStub = new Mock<ICourseService>();
+            courseServiceStub.Setup(cs => cs.GetAll()).Returns(listOfCourses.AsQueryable());
+
+            var mapperStub = new Mock<IMapper>();
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Course, CourseTileViewModel>();
+                cfg.CreateMap<CourseTileViewModel, Course>();
+            });
+
+            HomeController controller = new HomeController(courseServiceStub.Object, mapperStub.Object);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(3, ((HomeViewModel)result.ViewData.Model).Courses.Count());
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            var courseServiceStub = new Mock<ICourseService>();
+            var mapperStub = new Mock<IMapper>();
+
+            HomeController controller = new HomeController(courseServiceStub.Object, mapperStub.Object);
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void Contact_ShouldReturnNonNullResult()
+        {
+            // Arrange
+            var courseServiceStub = new Mock<ICourseService>();
+            var mapperStub = new Mock<IMapper>();
+            HomeController controller = new HomeController(courseServiceStub.Object, mapperStub.Object);
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
