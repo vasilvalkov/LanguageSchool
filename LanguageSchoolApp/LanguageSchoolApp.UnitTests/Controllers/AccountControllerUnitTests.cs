44527be9635259830a7788aebb33eaffd62f181a
vasilvalkov <vasil.valkov@gmail.com>
2017-10-06 15:01:30 +0300

    Added unit tests for ConfirmEmail in AccountController

--- LanguageSchoolApp/LanguageSchoolApp.UnitTests/Controllers/AccountControllerUnitTests.cs
+++ LanguageSchoolApp/LanguageSchoolApp.UnitTests/Controllers/AccountControllerUnitTests.cs
@@ -1,5 +1,9 @@
 ﻿using LanguageSchoolApp.Controllers;
+using LanguageSchoolApp.Services.Contracts;
+using Microsoft.AspNet.Identity;
+using Moq;
 using NUnit.Framework;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace LanguageSchoolApp.UnitTests.Controllers
@@ -19,6 +23,85 @@ namespace LanguageSchoolApp.UnitTests.Controllers
 
             // Assert
             Assert.AreEqual(returnUrl, result.ViewBag.ReturnUrl);
+        }
+
+        [Test]
+        public void ConfirmEmail_ShouldCallConfirmEmailMethodInUserManagementService()
+        {
+            // Arrange
+            string validUserId = "1234";
+            string validCode = "123";
+
+            var userManagerStub = new Mock<IUserManagementService>();
+            userManagerStub
+                .Setup(um => um.ConfirmEmailAsync(It.IsAny<string>(), It.IsAny<string>()));
+
+            var signInManagerMock = new Mock<ISignInService>();
+
+            var controller = new AccountController(userManagerStub.Object, signInManagerMock.Object);
+
+            // Act
+            var result = controller.ConfirmEmail(validUserId, validCode);
+
+            // Assert
+            userManagerStub.Verify(um => um.ConfirmEmailAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public void ConfirmEmail_ShouldReturnViewWithNameError_WhenConfirmationDoesNotSucceed()
+        {
+            // Arrange
+            string validUserId = "1234";
+            string validCode = "123";
+
+            var userManagerStub = new Mock<IUserManagementService>();
+            userManagerStub
+                .Setup(um => um.ConfirmEmailAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(Mock.Of<IdentityResult>());
+
+            var signInManagerMock = new Mock<ISignInService>();
+
+            var controller = new AccountController(userManagerStub.Object, signInManagerMock.Object);
+
+            // Act
+            var result = controller.ConfirmEmail(validUserId, validCode);
+
+            // Assert
+            Assert.AreEqual("Error", (result.Result as ViewResult).ViewName);
+        }
+
+        [Test]
+        public void ConfirmEmail_ShouldReturnViewWithNameError_WhenPassedUserIdIsNull()
+        {
+            // Arrange
+            string validCode = "123";
+            var userManagerStub = new Mock<IUserManagementService>();
+            var signInManagerMock = new Mock<ISignInService>();
+
+            var controller = new AccountController(userManagerStub.Object, signInManagerMock.Object);
+
+            // Act
+            var result = controller.ConfirmEmail(null, validCode);
+
+            // Assert
+            Assert.AreEqual("Error", (result.Result as ViewResult).ViewName);
+        }
+
+        [Test]
+        public void ConfirmEmail_ShouldReturnViewWithNameError_WhenPassedCodeIsNull()
+        {
+            // Arrange
+            string validUserId = "1234";
+            var userManagerStub = new Mock<IUserManagementService>();
+            var signInManagerMock = new Mock<ISignInService>();
+
+            var controller = new AccountController(userManagerStub.Object, signInManagerMock.Object);
+
+            // Act
+            var result = controller.ConfirmEmail(validUserId, null);
+
+            // Assert
+            Assert.AreEqual("Error", (result.Result as ViewResult).ViewName);
         }
 
         [Test]
