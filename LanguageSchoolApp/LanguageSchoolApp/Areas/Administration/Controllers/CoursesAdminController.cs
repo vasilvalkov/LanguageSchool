6c9d46fcff611733ef7fe3e21f1b175c5098a4d9
vasilvalkov <vasil.valkov@gmail.com>
2017-10-10 02:12:34 +0300

    Added Administration area

---
+++ LanguageSchoolApp/LanguageSchoolApp/Areas/Administration/Controllers/CoursesAdminController.cs
@@ -0,0 +1,58 @@
+﻿using LanguageSchoolApp.Areas.Administration.Models.Courses;
+using LanguageSchoolApp.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageSchoolApp.Areas.Administration.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CoursesAdminController : Controller
+    {
+        private readonly ICourseService courseService;
+        private readonly IUserService userService;
+
+        public CoursesAdminController(ICourseService courseService, IUserService userService)
+        {
+            if (courseService == null)
+            {
+                throw new ArgumentNullException("courseService");
+            }
+
+            if (userService == null)
+            {
+                throw new ArgumentNullException("userService");
+            }
+
+            this.courseService = courseService;
+            this.userService = userService;
+        }
+
+        // GET: Administration/Courses
+        public ActionResult Courses()
+        {
+            var courses = this.courseService.GetAll().ToList();
+
+            var viewModel = new CoursesAdminViewModel()
+            {
+                Courses = courses
+            };
+
+            return this.View(viewModel);
+        }
+
+
+        public JsonResult GetCourses()
+        {
+            if (this.Request.IsAjaxRequest())
+            {
+                var coursesJson = this.courseService.GetAll().ToList();
+                return Json(new { rows = coursesJson }, JsonRequestBehavior.AllowGet);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LanguageSchoolApp/LanguageSchoolApp/Areas/Administration/Models/Courses/CoursesAdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanguageSchoolApp.Data.Model;
+using System.Collections.Generic;
+
+namespace LanguageSchoolApp.Areas.Administration.Models.Courses
+{
+    public class CoursesAdminViewModel
+    {
+        public List<Course> Courses { get; set; }
+    }
+}
