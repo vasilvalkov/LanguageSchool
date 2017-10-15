2a8b6bc8099eb7baf46c98b4b7150772a08a9413
vasilvalkov <vasil.valkov@gmail.com>
2017-10-16 01:31:47 +0300

    Added unit tests for Admin controller

---
+++ LanguageSchoolApp/LanguageSchoolApp.UnitTests/Areas/Administration/Controllers/AdminControllerUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using LanguageSchoolApp.Areas.Administration.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace LanguageSchoolApp.UnitTests.Areas.Administration.Controllers
+{
+    [TestFixture]
+    public class AdminControllerUnitTests
+    {
+        [Test]
+        public void Index_ShouldReturnNonNullResult()
+        {
+            // Arrange
+            AdminController controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
+        [Test]
+        public void Index_ShouldReturnDefaultView()
+        {
+            // Arrange
+            AdminController controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(string.Empty, result.ViewName);
+        }
+    }
+}
